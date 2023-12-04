f0339c942978eda3b40d34e822a8af44762f0ab9
Pei-Hsuan Lin <alice70106@gmail.com>
2023-12-04 13:12:18 -0800

    finish decapitator and one shot test

--- src/decapitator.py
+++ src/decapitator.py
@@ -1,9 +1,27 @@
 import pandas as pd
 
-def decapitator(file, rows=[2, 3]):
-    print(rows)
-    for i in range(len(rows)):
-        rows[i] -= 1
-    print(rows)
+def decapitator(file, rows=None):
+    """
+    Remove specified rows from a DataFrame.
+
+    Parameters:
+    - file (pandas.DataFrame): The input DataFrame.
+    - rows (list, optional): List of row indices to remove. If None, default rows [1, 2] are removed.
+
+    Returns:
+    pandas.DataFrame: A new DataFrame with specified rows removed.
+
+    Example:
+    >>> import pandas as pd
+    >>> data = {'A': [1, 2, 3], 'B': [4, 5, 6], 'C': [7, 8, 9]}
+    >>> df = pd.DataFrame(data)
+    >>> result = decapitator(df, rows=[1])
+    """
+    if rows == None:
+        rows = [1, 2]
+    else:
+        for i in range(len(rows)):
+            rows[i] -= 1
     file = file.drop(labels=rows, axis=0)
+    file = file.reset_index(drop=True)
     return file
--- test/test_decapitator.py
+++ test/test_decapitator.py
@@ -10,27 +10,27 @@ class TestDecapitator(unittest.TestCase):
         df = pd.read_csv(raw_file_path)
         decapitator.decapitator(file=df)
     
+    # one shot test
     def test_decapitator_result(self):
         raw_file_path = os.path.join(os.path.dirname(__file__), "data", "vt3 raw data.csv")
-        raw_df = pd.read_csv(raw_file_path)
+        raw_df = pd.read_csv(raw_file_path, header=None)
         modified_file_path = os.path.join(os.path.dirname(__file__), "data", "vt3_decapitator_one_shot_test.csv")
-        modified_df = pd.read_csv(modified_file_path)
-        decapitator.decapitator(file=raw_df)
+        modified_df = pd.read_csv(modified_file_path, header=None)
+        raw_df = decapitator.decapitator(file=raw_df)
         self.assertIsInstance(modified_df, pd.DataFrame)
-        self.assertFalse(raw_df.shape == modified_df.shape)
-        self.assertFalse(raw_df.columns.equals(modified_df.columns))
+        self.assertTrue(raw_df.shape == modified_df.shape)
         comparison_result = raw_df.equals(modified_df)
         self.assertTrue(comparison_result)
-        
+    
+    # one shot test
     def test_decapitator_result_2(self):
         raw_file_path = os.path.join(os.path.dirname(__file__), "data", "vt3 raw data.csv")
-        raw_df = pd.read_csv(raw_file_path)
+        raw_df = pd.read_csv(raw_file_path, header=None)
         modified_file_path = os.path.join(os.path.dirname(__file__), "data", "vt3_decapitator_one_shot_test_2.csv")
-        modified_df = pd.read_csv(modified_file_path)
-        decapitator.decapitator(file=raw_df, rows=[1, 3])
+        modified_df = pd.read_csv(modified_file_path, header=None)
+        raw_df = decapitator.decapitator(file=raw_df, rows=[1, 3])
         self.assertIsInstance(modified_df, pd.DataFrame)
-        self.assertFalse(raw_df.shape == modified_df.shape)
-        self.assertFalse(raw_df.columns.equals(modified_df.columns))
+        self.assertTrue(raw_df.shape == modified_df.shape)
         comparison_result = raw_df.equals(modified_df)
         self.assertTrue(comparison_result)
 
