bbc9ca812277d30a044dbc3787dcfda8b12aa789
Sharon Wang <sharonwang.ssw@gmail.com>
2023-12-05 13:48:22 -0800

    adding one last test

--- test/test_reader.py
+++ test/test_reader.py
@@ -4,8 +4,8 @@ This module contains tests for the Reader component.
 
 import os
 import unittest
-import pandas as pd
 import time
+import pandas as pd
 from src import reader
 
 class TestReader(unittest.TestCase):
@@ -37,17 +37,17 @@ class TestReader(unittest.TestCase):
 
         result = df1.equals(df2)
         self.assertTrue(result)
-    
+
     def test_file_exists(self):
         """
         Test to ensure the file specified exists.
         """
-        
+
         path = "imaginary_file.txt"
-        
-        with self.assertRaises(FileNotFoundError) as context:
+
+        with self.assertRaises(FileNotFoundError):
             reader.reader(path)
-    
+
     def test_file_type(self):
         """
         Test to ensure file type is csv.
@@ -56,10 +56,15 @@ class TestReader(unittest.TestCase):
         path = os.path.join(os.path.dirname(__file__), "data",
                             "readme.txt")
 
-        with self.assertRaises(ValueError) as context:
+        with self.assertRaises(ValueError):
             reader.reader(path)
 
     def test_large_file_performance(self):
+        """
+        Test to ensure file is not too long and processing won't take
+        too long.
+        """
+
         # Create a temporary large CSV file for testing
         large_csv_file_path = "large_file.csv"
 
@@ -72,14 +77,14 @@ class TestReader(unittest.TestCase):
 
         # Measure the time it takes to process the large file
         start_time = time.time()
-        result_df = pd.read_csv(large_csv_file_path)
+        result_df = reader.reader(large_csv_file_path)
         end_time = time.time()
 
         # Assert that the result is a DataFrame and the processing time is reasonable
         self.assertIsInstance(result_df, pd.DataFrame)
         processing_time = end_time - start_time
-        print(f"Processing time for {num_rows} rows and {num_columns} columns: {processing_time:.2f} seconds")
-        
+        print(f"Processing time: {processing_time:.2f} seconds")
+
         # Optionally, set a threshold for processing time based on your performance requirements
         threshold_time = 10.0  # Set your threshold time in seconds
         self.assertLess(processing_time, threshold_time)
