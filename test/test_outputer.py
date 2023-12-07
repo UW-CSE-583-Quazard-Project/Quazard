ab09daabe128e88b668be93763166d7c648956c6
Pei-Hsuan Lin <alice70106@gmail.com>
2023-12-07 12:16:07 -0800

    debug outputer tests

--- test/test_outputer.py
+++ test/test_outputer.py
@@ -7,8 +7,10 @@ class TestOutputer(unittest.TestCase):
     # smoke test:
     def test_outputer(self):
         raw_file_path = os.path.join(os.path.dirname(__file__), "data", "vt3 raw data.csv")
-        test_df = pd.read_csv(raw_file_path)
-        outputer.outputer(test_df, raw_file_path)
+        csv_file_path = os.path.join(os.path.dirname(__file__), "data", "outputer_test_file.csv")
+        test_df = pd.read_csv(raw_file_path, header=None)
+        outputer.outputer(test_df, csv_file_path)
+        os.remove(csv_file_path)
     
     # one shot test
     def test_outputer_result(self):
