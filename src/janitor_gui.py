e98115c3f4a3c6e4b8ff2a915e49a59d35b1a008
llamazhi <zhongz22@uw.edu>
2023-12-07 16:14:49 -0800

    Finished basic janitor component gui; The user can now select expected columns and drop them by GUI

--- src/janitor_gui.py
+++ src/janitor_gui.py
@@ -1,20 +1,53 @@
+import tkinter as tk
 import tkinter.ttk as ttk
-
-def create_janitor_tab(tab_control):
+import janitor
+import pandas as pd
+from tkinter import messagebox
+def create_janitor_tab(tab_control, dataframe=None):
     janitor_tab = ttk.Frame(tab_control)
     tab_control.add(janitor_tab, text='Janitor')
     
-    # Create text entry widgets within the frame
-    label1 = ttk.Label(janitor_tab, text="Entry 1:")
-    entry1 = ttk.Entry(janitor_tab)
+    # Test dataframe
+    df = pd.read_csv("../test/data/vt3 data cleaned not converted or aggregated.csv")
+    columns = df.columns.tolist()
+    
+    # Create a box to display selected columns
+    listbox = tk.Listbox(janitor_tab)
+    listbox.pack(pady=10)
+    
+    # Create a dropmenu with options of dataframe columns
+    selected_values = []
+    combobox = ttk.Combobox(janitor_tab, values=columns)
+    combobox.bind("<<ComboboxSelected>>", lambda event, cb=combobox: add_to_list(cb, selected_values, listbox))
+    combobox.pack(padx=20, pady=20)
+    
+    # Create a button to submit selected values
+    drop_button = ttk.Button(janitor_tab, text="Drop Selected Columns", command=lambda: drop_selected_columns(selected_values, df))
+    drop_button.pack(pady=10)
+    
+    # Create a button to clear selected columns
+    clear_button = ttk.Button(janitor_tab, text="Clear List", command=lambda: clear_list(listbox))
+    clear_button.pack(pady=10)
+    
+def add_to_list(combobox, selected_values, listbox):
+    selected_value = combobox.get()
+    if selected_value not in selected_values:
+        selected_values.append(selected_value)
+        update_listbox(selected_values, listbox)
 
-    label1.pack(padx=10, pady=10)
-    entry1.pack(pady=10)
+def update_listbox(selected_values, listbox):
+    listbox.delete(0, tk.END)
+    for value in selected_values:
+        listbox.insert(tk.END, value)
+        
+def clear_list(listbox):
+    listbox.delete(0, tk.END)
     
-    label2 = ttk.Label(janitor_tab, text="Entry 2:")
-    entry2 = ttk.Entry(janitor_tab)
-
-    label2.pack(padx=10, pady=10)
-    entry2.pack(pady=10)
-    
-    
+def drop_selected_columns(selected_values, dataframe):
+    # Check if there are columns selected
+    if selected_values:
+        new_dataframe = janitor.drop_cols(dataframe, selected_values)
+        if len(new_dataframe.columns) != len(dataframe.columns):
+            messagebox.showinfo("Columns Dropped", "Selected columns have been dropped.")
+        else:
+            messagebox.showwarning("Dropping Failed", "Selected columns have not beed dropped properly!")
