fe00bbbe2d821f5309187e3d9e023bf3c5c647d1
llamazhi <zhongz22@uw.edu>
2023-12-12 13:24:17 -0800

    Changed the strucutre of GUI to class-based App

--- src/decapitator_gui.py
+++ src/decapitator_gui.py
@@ -3,40 +3,48 @@ import tkinter.ttk as ttk
 import decapitator
 import re
 
-def is_valid_input(user_input):
-    # Use a regular expression to check if the input contains characters other than numbers and commas
-    if len(user_input) == 0:
-        return False
-    pattern = re.compile(r'[^1-3,]')
-    return not bool(pattern.search(user_input))
-
-def decapitator_function(dataframe, rows, submit_button, result_label):
-    submit_button.configure(state="disabled")
-    if not is_valid_input(rows) :
-        rows = None
-        result_label.config(text="The default redundant headers are removed")
-    else:
-        rows = rows.split(",")
-        rows = [int(s) for s in rows]
-        print(rows)
-        result_label.config(text="Redundant headers are removed")
-    decapitator_result = decapitator.decapitator(dataframe, rows)
-    return decapitator
-
-def create_decapitator_tab(tab_control, dataframe=None):
-    decapitator_tab = ttk.Frame(tab_control)
-    tab_control.add(decapitator_tab, text='Decapitator')
+class DecapitatorGUI:
+    def __init__(self):
+        pass
     
-    label_decapitator = ttk.Label(decapitator_tab, text="This is the Decapitator Tab. You can choose which redundant header that you want to delete. By default it delete the second and the third row. But if you want to delete the first row and the third row, please enter 1,3 without any space")
-    label_decapitator.pack(padx=10, pady=10)
+    def is_valid_input(self, user_input):
+        # Use a regular expression to check if the input contains characters other than numbers and commas
+        if len(user_input) == 0:
+            return False
+        pattern = re.compile(r'[^1-3,]')
+        return not bool(pattern.search(user_input))
 
-    entry_text = tk.StringVar()
-    entry = ttk.Entry(decapitator_tab, textvariable=entry_text)
-    entry.pack(pady=10)
+    def decapitator_function(self, dataframe, rows, submit_button, result_label):
+        submit_button.configure(state="disabled")
+        if not self.is_valid_input(rows) :
+            rows = None
+            result_label.config(text="The default redundant headers are removed")
+        else:
+            rows = rows.split(",")
+            rows = [int(s) for s in rows]
+            print(rows)
+            result_label.config(text="Redundant headers are removed")
+        decapitator_result = decapitator.decapitator(dataframe, rows)
+        return decapitator
 
-    # Add a Label for displaying the result
-    result_label = ttk.Label(decapitator_tab, text="")
-    result_label.pack(pady=10)
+    def create_decapitator_tab(self, tab_control, dataframe=None):
+        decapitator_tab = ttk.Frame(tab_control)
+        tab_control.add(decapitator_tab, text='Decapitator')
+        
+        label_decapitator = ttk.Label(decapitator_tab, text="This is the Decapitator Tab. You can choose which redundant header that you want to delete. By default it delete the second and the third row. But if you want to delete the first row and the third row, please enter 1,3 without any space")
+        label_decapitator.pack(padx=10, pady=10)
 
-    submit_button = ttk.Button(decapitator_tab, text='Submit', command=lambda: decapitator_function(dataframe, entry_text.get(), submit_button, result_label))
-    submit_button.pack(pady=10)
+        entry_text = tk.StringVar()
+        entry = ttk.Entry(decapitator_tab, textvariable=entry_text)
+        entry.pack(pady=10)
+
+        # Add a Label for displaying the result
+        result_label = ttk.Label(decapitator_tab, text="")
+        result_label.pack(pady=10)
+
+        submit_button = ttk.Button(decapitator_tab, text='Submit', command=lambda: self.decapitator_function(dataframe, entry_text.get(), submit_button, result_label))
+        submit_button.pack(pady=10)
+        
+        # TODO
+        # Add appropriate dataframe to be returned here
+        
--- src/gui.py
+++ src/gui.py
@@ -1,25 +1,38 @@
+"""
+This is the main module for our application GUI
+"""
 import tkinter as tk
 from tkinter import ttk, simpledialog, messagebox
-# import pandas as pd
-# import numpy as np
 import reader_gui, decapitator_gui, executioner_gui, longwideconverter_gui, janitor_gui, frankenstein_gui, outputer_gui
+
+class App:
+    def __init__(self):
+        self.dataframe = None
+        
+    def run(self):
+        root = tk.Tk()
+        root.title('Seven Tabbed GUI')
+        tab_control = ttk.Notebook(root)
+        reader = reader_gui.ReaderGUI()
+        decapitator = decapitator_gui.DecapitatorGUI()
+        
+        reader.create_reader_tab(tab_control)
+        # reader_gui.create_reader_tab(tab_control)
+        decapitator.create_decapitator_tab(tab_control)
+        
+        executioner_gui.create_executioner_tab(tab_control)
+        longwideconverter_gui.create_converter_tab(tab_control)
+        janitor_gui.create_janitor_tab(tab_control)
+        frankenstein_gui.create_frankenstein_tab(tab_control)
+        outputer_gui.create_outputer_tab(tab_control)
+        tab_control.pack(expand=1, fill='both')
+        root.mainloop()
+
+
 def main():
-    root = tk.Tk()
-    root.title('Seven Tabbed GUI')
+    app = App()
+    app.run()
     
-    tab_control = ttk.Notebook(root)
-    
-    reader_gui.create_reader_tab(tab_control)
-    decapitator_gui.create_decapitator_tab(tab_control)
-    executioner_gui.create_executioner_tab(tab_control)
-    longwideconverter_gui.create_converter_tab(tab_control)
-    janitor_gui.create_janitor_tab(tab_control)
-    frankenstein_gui.create_frankenstein_tab(tab_control)
-    outputer_gui.create_outputer_tab(tab_control)
-
-    tab_control.pack(expand=1, fill='both')
-
-    root.mainloop()
 
 if __name__ == "__main__":
     main()
