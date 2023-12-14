436d63aa27ae3f34c0860f67d15f8b2042b2a0a8
robertqin525 <robertqin525@gmail.com>
2023-12-13 21:05:59 -0800

    Added a new function to reader to read data that does not need to go through the decapitator

--- src/reader_gui.py
+++ src/reader_gui.py
@@ -29,8 +29,19 @@ class ReaderGUI:
         label_file_name = ttk.Label(reader_tab, text=f'File Name: "Test"')
         label_file_name.pack(pady=10)
 
+        label_reader_threeheader = ttk.Label(reader_tab, text='Use this button if your data has three rows of header \n'
+                                             ' and will need to go through the decapitator')
+        label_reader_threeheader.pack(padx=10, pady=10)
+
         button_reader = ttk.Button(reader_tab, text='Select File', command=self.on_button_click)
-        button_reader.pack(pady=10)        
+        button_reader.pack(pady=10)
+
+        label_reader_oneheader = ttk.Label(reader_tab, text='Use this button if your data has one row of header \n'
+                                             ' and will not go through the decapitator')
+        label_reader_oneheader.pack(padx=10, pady=10)
+
+        button_reader_header = ttk.Button(reader_tab, text='Select File', command=self.on_button_click_one_header)
+        button_reader_header.pack(pady=10)              
         
     def on_button_click(self):
         filetypes = (('csv files', '*.csv'),
@@ -40,4 +51,14 @@ class ReaderGUI:
             initialdir='/',
             filetypes=filetypes)
         dataframe = rd.reader(filename)
+        self.app_instance.update_dataframe(dataframe)
+
+    def on_button_click_one_header(self):
+        filetypes = (('csv files', '*.csv'),
+                    ('All files', '*.*'))
+        filename = fd.askopenfilename(
+            title='Open a file',
+            initialdir='/',
+            filetypes=filetypes)
+        dataframe = rd.reader_oneheader(filename)
         self.app_instance.update_dataframe(dataframe)
